673d628265075e318ebb13fb6fbd914a9b17a2a5
chetan <chetan_madnaik@neovasolutions.com>
2016-09-02 16:47:33 +0530

    #4 new print

--- src/com/neova/sds/Calculator.java
+++ src/com/neova/sds/Calculator.java
@@ -17,14 +17,11 @@ public class Calculator {
 		
 		Multiplication multiplication =new Multiplication();
 		System.out.println("multiplication result :"+multiplication.multiplication(3, 5));
-		
-		
-		Square sq =new Square();
-		System.out.println(sq.square(3));
 		//		Calling calculateDivision() method of Division class
 		int a = 50, b= 25;
 		Division division = new Division();
 		int divRes = division.calculateDivision(a, b);
 		System.out.println("Division of a by b is: "+divRes);
+		System.out.println("i am  drecco9");
 	}
 }
