27b065842a196388b8fd6912376f257b48befeca
Ashish Jagtap <ashish_jagtap@neovasolutions.com>
2016-09-02 11:59:25 +0530

    Adding basic project structure for calculator project.

---
+++ src/com/neova/sds/Calculator.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.neova.sds;
+
+/**
+ * @author Ashish
+ *
+ */
+public class Calculator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
